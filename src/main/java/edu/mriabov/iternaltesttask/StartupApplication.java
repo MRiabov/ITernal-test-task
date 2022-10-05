a1b0a895a0867aa28013f6e2fe241a0123affe66
MRiabov <maksymriabov2004@gmail.com>
2022-10-05 20:48:54 +0200

    directory automatically created

--- src/main/java/edu/mriabov/iternaltesttask/StartupApplication.java
+++ src/main/java/edu/mriabov/iternaltesttask/StartupApplication.java
@@ -1,5 +1,6 @@
 package edu.mriabov.iternaltesttask;
 
+import edu.mriabov.iternaltesttask.config.Config;
 import edu.mriabov.iternaltesttask.csv.CsvReader;
 import edu.mriabov.iternaltesttask.csv.CsvParser;
 import edu.mriabov.iternaltesttask.xml.XMLCreatorService;
@@ -8,18 +9,22 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+
 @Component
 @RequiredArgsConstructor
 public class StartupApplication implements ApplicationListener<ContextRefreshedEvent> {
 
     private final XMLCreatorService xmlCreatorService;
-    private final CsvParser CSVParser;
-    private final CsvReader CSVReader;
+    private final CsvParser csvParser;
+    private final Config config;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        new File(config.getOutputPath()).mkdirs();
+        new File(config.getInvalidOutputPath()).mkdirs();
         xmlCreatorService.fillMultipleXML(
-                CSVParser.parseData().getRightValue());
+                csvParser.parseData().getRightValue());
     }
 
 }
